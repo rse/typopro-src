ea942ac5eb7d035cbc32a5624b25f17ee40e1a94
Ralf S. Engelschall <rse@engelschall.com>
2015-07-19 20:21:21 +0200

    fix path calculations

--- etc/manifest.pl
+++ etc/manifest.pl
@@ -33,8 +33,9 @@ my $json = "TypoPRO_Manifest = [\n";
 my $txt < io($ARGV[0]);
 foreach my $line (split(/\n/, $txt)) {
     my ($css) = ($line =~ m/^(.+?)\.(?:otf|ttf).*/);
-    $css =~ s/^(.*)\/(.*)/$ARGV[1]\/TypoPRO-$1\/TypoPRO-$2.css/s;
-    my $txt < io($css);
+    my $css1 = $css; $css1 =~ s/^(.*)\/(.*)/$ARGV[1]\/TypoPRO-$1\/TypoPRO-$2.css/s;
+    my $css2 = $css; $css2 =~ s/^(.*)\/(.*)/web\/TypoPRO-$1\/TypoPRO-$2.css/s;
+    my $txt < io($css1);
     my ($font_name)    = ($txt =~ m/\/\*\s+(.+?)\s+\*\//s);
     my ($font_family)  = ($txt =~ m/font-family:\s+'([^']+)'/s);
     my ($font_style)   = ($txt =~ m/font-style:\s+(.+?);/s);
@@ -48,7 +49,7 @@ foreach my $line (split(/\n/, $txt)) {
         "\"weight\": \"$font_weight\", " .
         "\"stretch\": \"$font_stretch\", " .
         "\"variant\": \"$font_variant\", " .
-        "\"css\": \"$css\" " .
+        "\"css\": \"$css2\" " .
     "},\n";
 }
 $json =~ s/,\n$/\n/s;
